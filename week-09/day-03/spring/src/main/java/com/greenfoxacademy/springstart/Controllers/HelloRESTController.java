339f621d30bae86e632f1f45f191aa7e687fda11
Oparin-Salamon Melinda <melinda@oparin.hu>
2018-06-02 15:31:18 +0200

    Intro to Spring - Hello User REST App exercise

--- week-09/day-03/spring/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloRESTController.java
+++ week-09/day-03/spring/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloRESTController.java
@@ -1,13 +1,15 @@
 package com.greenfoxacademy.springstart.Controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
     @RequestMapping(value = "/greeting")
-    public Greeting greet() {
-        return new Greeting(1, "Hello");
+    public Greeting greet(@RequestParam("name") String name) {
+        return new Greeting(1, "Hello " + name);
     }
 }
