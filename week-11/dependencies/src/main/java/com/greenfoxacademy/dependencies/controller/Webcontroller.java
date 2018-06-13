7c2548ffdc65d1b95b84d37247123aac137e6213
Oparin-Salamon Melinda <melinda@oparin.hu>
2018-06-13 20:22:26 +0200

    Using dependencies - Useful utilities (Ceasar encoder/decoder)

--- week-11/dependencies/src/main/java/com/greenfoxacademy/dependencies/controller/Webcontroller.java
+++ week-11/dependencies/src/main/java/com/greenfoxacademy/dependencies/controller/Webcontroller.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class Webcontroller {
     UtilityService utilityService;
@@ -34,4 +36,20 @@ public class Webcontroller {
         model.addAttribute("email", email);
         return "validateemail";
     }
+
+    @RequestMapping("useful/encoder")
+    public String encoder(@RequestParam("text") String text,
+        @RequestParam("number") int number, Model model) {
+        model.addAttribute("encode", utilityService.caesar(text, number));
+        return "encoder";
+    }
+
+    @RequestMapping("useful/decoder")
+    public String decoder(@RequestParam("text") String text,
+                          @RequestParam("number") int number, Model model) {
+        model.addAttribute("decode", utilityService.caesar(text, number * (-1)));
+        return "decoder";
+    }
+
+
 }
