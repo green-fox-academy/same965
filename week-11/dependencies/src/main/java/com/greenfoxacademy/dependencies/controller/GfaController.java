1e77f4340fe4baee8e1651c366615bd384f21e86
Oparin-Salamon Melinda <melinda@oparin.hu>
2018-06-15 18:53:35 +0200

    Using dependencies - Greenfox Class App (Adding new students)

--- week-11/dependencies/src/main/java/com/greenfoxacademy/dependencies/controller/GfaController.java
+++ week-11/dependencies/src/main/java/com/greenfoxacademy/dependencies/controller/GfaController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GfaController {
@@ -27,4 +30,14 @@ public class GfaController {
         return "list";
     }
 
+    @GetMapping("gfa/add")
+    public String addStudent() {
+        return "addstudent";
+    }
+
+    @PostMapping("gfa/save")
+    public String saveNewStudent(@ModelAttribute("name") String name) {
+        studentService.save(name);
+        return "redirect:/gfa/list";
+    }
 }
