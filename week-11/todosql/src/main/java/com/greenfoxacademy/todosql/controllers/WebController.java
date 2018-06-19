4ef8aade356ac79c782ad4cdd8c9d6e2b4032d07
Oparin-Salamon Melinda <melinda@oparin.hu>
2018-06-19 15:34:08 +0200

    Database Integration - Listing todos MySQL

--- week-11/todosql/src/main/java/com/greenfoxacademy/todosql/controllers/WebController.java
+++ week-11/todosql/src/main/java/com/greenfoxacademy/todosql/controllers/WebController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.todosql.controllers;
 
-import com.greenfoxacademy.todosql.repositories.TodoRepository;
 import com.greenfoxacademy.todosql.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller(value = "todo")
+@Controller
 public class WebController {
 
     public final TodoService todoService;
@@ -27,5 +28,11 @@ public class WebController {
     public String list(Model model) {
         model.addAttribute("todos", todoService.listTodos());
         return "todoslist";
+    }
+
+    @RequestMapping("todo/")
+    public String listUndone(@RequestParam("isActive") boolean isActive, Model model) {
+        model.addAttribute("undone", todoService.listUnDoneTodos(isActive));
+        return "undonelist";
     }
 }
--- week-11/todosql/src/main/java/com/greenfoxacademy/todosql/services/TodoService.java
+++ week-11/todosql/src/main/java/com/greenfoxacademy/todosql/services/TodoService.java
@@ -1,4 +1,47 @@
 package com.greenfoxacademy.todosql.services;
 
+import com.greenfoxacademy.todosql.models.Todo;
+import com.greenfoxacademy.todosql.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TodoService {
+
+    public final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public void add(Todo todo) {
+        todoRepository.save(todo);
+    }
+
+    public void create() {
+        add(new Todo("finish this exercise", true, false));
+        add(new Todo("really finish this exercise", true, false));
+        add(new Todo("rewrite this class, bah", true, true));
+        add(new Todo("finish all data integration exercise", true, false));
+        add(new Todo("start programmer fox club exercise", false, false));
+    }
+
+    public List<Todo> listTodos() {
+        return (List<Todo>) todoRepository.findAll();
+    }
+
+    public List<Todo> listUnDoneTodos(boolean done) {
+        List<Todo> undoneTodos = new ArrayList<>();
+        List<Todo> allTodos = listTodos();
+        for (Todo todo: allTodos) {
+            if (todo.getDone() != done) {
+                undoneTodos.add(todo);
+            }
+        }
+        return undoneTodos;
+    }
 }
